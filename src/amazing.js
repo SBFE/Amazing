dd914d15a51e5c03d0c733588d550e36b947a039
Xiaojue <designsor@gmail.com>
2015-01-28 17:42:20 +0800

    重构api

--- src/amazing.js
+++ src/amazing.js
@@ -8,37 +8,109 @@
     }
   };
 
-  var setStyle = function(node, source, to, t, duration) {
-    for (var i in to) {
-      if(!source[i]){
-         source[i] = (global.getComputedStyle ? getComputedStyle(node, null) : node.currentStyle)[i];
+  var RGBA = /#(.)(.)(.)\b|#(..)(..)(..)\b|(\d+)%,(\d+)%,(\d+)%(?:,([\d\.]+))?|(\d+),(\d+),(\d+)(?:,([\d\.]+))?\b/;
+  var toRGBA = function(s, v) {
+    v = [0, 0, 0, 0];
+    s.replace(/\s/g, "").replace(RGBA, function(i, a, b, c, f, g, h, l, m, n, o, w, x, y, z) {
+      h = [a + a || f, b + b || g, c + c || h];
+      p = [l, m, n];
+
+      for (i = 0; i < 3; i++) {
+        h[i] = parseInt(h[i], 16);
+        p[i] = Math.round(p[i] * 2.55);
       }
-      var units = ((/\d(\D+)$/).exec(to[i]) || (/\d(\D+)$/).exec(source[i]) || [0, 0])[1];  //units (px, %)
-      var curto = parseInt(to[i],10);
-      var curso = parseInt(source[i],10);
-      var target = easeings['swing']((1 - (t / duration)).toFixed(1), duration - t, 0, curto - curso, duration);
-      var setvalue = parseInt(curso + target, 10);
-      setvalue = setvalue > curto ? curto : setvalue;
-      setvalue += units;
+
+      v = [h[0] || p[0] || w || 0, h[1] || p[1] || x || 0, h[2] || p[2] || y || 0, o || z || 1];
+    });
+    return v;
+  };
+
+  function getSourceStyle(node,style){
+      return (global.getComputedStyle ? getComputedStyle(node, null) : node.currentStyle)[style];
+  }
+
+  var setStyle = function(node, source, to, t, duration) {
+    source = source ? source: {};
+    var curto, curso, target, setvalue, units;
+    for (var i in to) {
+      if (!source[i]) {
+        source[i] = getSourceStyle(node,i);
+      }
+      //判断透明度，color
+      if ((/color/i).test(i)) { //只转换一次,做标记
+        curto = toRGBA(to[i]);
+        curso = toRGBA(source[i]);
+        if (curto[3] === 0) {
+          curto = [].concat(curso);
+          curto[3] = 0;
+        }
+        if (curso[3] === 0) {
+          curso = [].concat(curto);
+          curso[3] = 0;
+        }
+      } else {
+        units = ((/\d(\D+)$/).exec(to[i]) || (/\d(\D+)$/).exec(source[i]) || [0, 0])[1]; //units (px, %)
+        curto = parseInt(to[i], 10);
+        curso = parseInt(source[i], 10);
+        target = easeings['swing']((1 - (t / duration)).toFixed(1), duration - t, 0, curto - curso, duration);
+        setvalue = parseInt(curso + target, 10);
+        setvalue = setvalue > curto ? curto: setvalue;
+        setvalue += units;
+      }
       node.style[i] = setvalue;
     }
   };
 
-  var anim = function(cb) {
-    var item = queue.shift();
-    var endTime = new Date().valueOf() + item.duration;
-    var _ = function() {
-      var now = new Date().valueOf();
-      var t = endTime - now;
-      setStyle(item.node, item.source, item.to, t, item.duration);
-      if (t < 0) {
-        cb(item);
-      } else {
-        setTimeout(_, 16);
+  function events() {
+    this.map = {};
+  }
+
+  events.prototype = {
+    constructor: events,
+    trigger: function(name, args) {
+      var self = this,
+      cbs = this.map[name];
+      if (cbs) {
+        forEach(cbs, function(fn) {
+          fn.apply(this, args);
+        });
       }
-    };
-    _();
+    },
+    on: function(name, cb) {
+      if (this.map[name]) {
+        this.map[name].push(cb);
+      } else {
+        this.map[name] = [cb];
+      }
+    }
   };
+
+  //https://gist.github.com/paulirish/1579671 
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for (var x = 0; x < vendors.length && ! window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function(callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() {
+        callback(currTime + timeToCall);
+      },
+      timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  }
+
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
+  }
 
   var exts = ['Top', 'Left', 'Bottom', 'Right'];
 
@@ -79,28 +151,90 @@
   var batch = function(item) {
     expand(item.source);
     expand(item.to);
-    //console.log(item.to);
   };
 
-  var queue = [];
-
-  var amazing = function(node, params, callback) {
-    var queueItem = {
-      node: node,
+  var configure = function(params){
+    return {
+      node: params.node,
       source: params.source,
       to: params.to,
       duration: params.duration || 500,
       ease: params.ease || 'swing',
-      cb: callback
+      cb: params.callback
     };
-    batch(queueItem);
-    queue.push(queueItem);
-    anim(function(item) {
-      var next = queue.shift();
-      if (next) anim(next);
-      else callback();
-    });
   };
-  global.amazing = amazing;
-})(this);
 
+  function clone(obj) {
+    // Handle the 3 simple types, and null or undefined
+    if (null === obj || "object" != typeof obj) return obj;
+    var copy;
+    // Handle Date
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        copy = [];
+        for (var i = 0, len = obj.length; i < len; ++i) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+  }
+
+  function Amazing(params) {
+    this.events = new events();
+    this.queueItem = configure(params);
+    this.timer = null;
+  }
+
+  Amazing.prototype = {
+    constructor: Amazing,
+    stop: function() {
+      if(this.timer){
+        cancelAnimationFrame(this.timer);
+        this.events.trigger('stop');
+      }
+    },
+    replay: function() {
+      batch(this.queueItem);
+      this.play();
+      this.events.trigger('restart');
+    },
+    play: function() {
+      var item = this.queueItem,
+      endTime = new Date().valueOf() + item.duration,
+      self = this;
+      var _ = function() {
+        var now = new Date().valueOf();
+        var t = endTime - now;
+        setStyle(item.node, item.source, item.to, t, item.duration);
+        self.events.trigger('move',[t,item.source,item.to]);
+        if (t < 0) {
+          if (item.cb) item.cb();
+          self.events.trigger('end');
+        } else {
+          self.timer = requestAnimationFrame(_);
+        }
+      };
+      _();
+    }
+  };
+
+  global.Amazing = Amazing;
+
+})(this);
