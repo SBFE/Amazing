131072771e98ee094f73df307a4a0fa31e59fc8a
Xiaojue <designsor@gmail.com>
2015-02-05 11:11:10 +0800

    color变换完成

--- src/amazing.js
+++ src/amazing.js
@@ -26,7 +26,8 @@
     return true;
   } ();
 
-  //hasCss3 = false;
+  hasCss3 = false;
+
   var easeings = {
     swing: function(x, t, b, c, d) {
       return this.easeInQuad(x, t, b, c, d);
@@ -36,21 +37,92 @@
     }
   };
 
-  var RGBA = /#(.)(.)(.)\b|#(..)(..)(..)\b|(\d+)%,(\d+)%,(\d+)%(?:,([\d\.]+))?|(\d+),(\d+),(\d+)(?:,([\d\.]+))?\b/;
-  var toRGBA = function(s, v) {
-    v = [0, 0, 0, 0];
-    s.replace(/\s/g, "").replace(RGBA, function(i, a, b, c, f, g, h, l, m, n, o, w, x, y, z) {
-      h = [a + a || f, b + b || g, c + c || h];
-      p = [l, m, n];
+  // Parse strings looking for color tuples [255,255,255]
+  function getRGB(color) {
+    var result;
 
-      for (i = 0; i < 3; i++) {
-        h[i] = parseInt(h[i], 16);
-        p[i] = Math.round(p[i] * 2.55);
-      }
+    // Check if we're already dealing with an array of colors
+    if (color && color.constructor == Array && color.length == 3) return color;
 
-      v = [h[0] || p[0] || w || 0, h[1] || p[1] || x || 0, h[2] || p[2] || y || 0, o || z || 1];
-    });
-    return v;
+    // Look for rgb(num,num,num)
+    result = /rgb\(\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*,\s*([0-9]{1,3})\s*\)/.exec(color);
+    if (result) {
+      return [parseInt(result[1], 10), parseInt(result[2], 10), parseInt(result[3], 10)];
+    }
+
+    // Look for rgb(num%,num%,num%)
+    result = /rgb\(\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*,\s*([0-9]+(?:\.[0-9]+)?)\%\s*\)/.exec(color);
+    if (result) {
+      return [parseFloat(result[1]) * 2.55, parseFloat(result[2]) * 2.55, parseFloat(result[3]) * 2.55];
+    }
+
+    // Look for #a0b1c2
+    result = /#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})/.exec(color);
+    if (result) {
+      return [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)];
+    }
+
+    // Look for #fff
+    result = /#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])/.exec(color);
+    if (result) {
+      return [parseInt(result[1] + result[1], 16), parseInt(result[2] + result[2], 16), parseInt(result[3] + result[3], 16)];
+    }
+
+    // Look for rgba(0, 0, 0, 0) == transparent in Safari 3
+    result = /rgba\(0, 0, 0, 0\)/.exec(color);
+    if (result) {
+      return colors['transparent'];
+    }
+
+    // Otherwise, we're most likely dealing with a named color
+    return colors[color.toLowerCase()];
+  }
+
+  var colors = {
+    aqua: [0, 255, 255],
+    azure: [240, 255, 255],
+    beige: [245, 245, 220],
+    black: [0, 0, 0],
+    blue: [0, 0, 255],
+    brown: [165, 42, 42],
+    cyan: [0, 255, 255],
+    darkblue: [0, 0, 139],
+    darkcyan: [0, 139, 139],
+    darkgrey: [169, 169, 169],
+    darkgreen: [0, 100, 0],
+    darkkhaki: [189, 183, 107],
+    darkmagenta: [139, 0, 139],
+    darkolivegreen: [85, 107, 47],
+    darkorange: [255, 140, 0],
+    darkorchid: [153, 50, 204],
+    darkred: [139, 0, 0],
+    darksalmon: [233, 150, 122],
+    darkviolet: [148, 0, 211],
+    fuchsia: [255, 0, 255],
+    gold: [255, 215, 0],
+    green: [0, 128, 0],
+    indigo: [75, 0, 130],
+    khaki: [240, 230, 140],
+    lightblue: [173, 216, 230],
+    lightcyan: [224, 255, 255],
+    lightgreen: [144, 238, 144],
+    lightgrey: [211, 211, 211],
+    lightpink: [255, 182, 193],
+    lightyellow: [255, 255, 224],
+    lime: [0, 255, 0],
+    magenta: [255, 0, 255],
+    maroon: [128, 0, 0],
+    navy: [0, 0, 128],
+    olive: [128, 128, 0],
+    orange: [255, 165, 0],
+    pink: [255, 192, 203],
+    purple: [128, 0, 128],
+    violet: [128, 0, 128],
+    red: [255, 0, 0],
+    silver: [192, 192, 192],
+    white: [255, 255, 255],
+    yellow: [255, 255, 0],
+    transparent: [255, 255, 255]
   };
 
   function setToStyle(node, to) {
@@ -84,27 +156,23 @@
         source[i] = getSourceStyle(node, i);
       }
       //判断透明度,opacity,color,width
-      if ((/color/i).test(i)) { //只转换一次,做标记
-        curto = toRGBA(to[i]);
-        curso = toRGBA(source[i]);
-        if (curto[3] === 0) {
-          curto = [].concat(curso);
-          curto[3] = 0;
-        }
-        if (curso[3] === 0) {
-          curso = [].concat(curto);
-          curso[3] = 0;
-        }
+      if ((/color/i).test(i)) {
+        //只转换一次,做标记
+        curso = getRGB(source[i]);
+        curto = getRGB(to[i]);
+        target = {};
+        target.r = Math.abs(easeings[ease]((1 - (t / duration)).toFixed(1), duration - t, curso[0], curto[0] - curso[0], duration));
+        target.g = Math.abs(easeings[ease]((1 - (t / duration)).toFixed(1), duration - t, curso[1], curto[1] - curso[1], duration));
+        target.b = Math.abs(easeings[ease]((1 - (t / duration)).toFixed(1), duration - t, curso[2], curto[2] - curso[2], duration));
+        node.style[i] = 'rgb(' + parseInt(target.r, 10) + ',' + parseInt(target.g, 10) + ',' + parseInt(target.b, 10) + ')';
       } else {
         units = ((/\d(\D+)$/).exec(to[i]) || (/\d(\D+)$/).exec(source[i]) || [0, 0])[1]; //units (px, %)
         curto = parseInt(to[i], 10);
         curso = parseInt(source[i], 10);
-        target = easeings[ease]((1 - (t / duration)).toFixed(1), duration - t, 0, curto - curso, duration);
-        setvalue = parseInt(curso + target, 10);
-        //setvalue = setvalue > curto ? curto: setvalue;
-        setvalue += units;
+        target = easeings[ease]((1 - (t / duration)).toFixed(1), duration - t, curso, curto - curso, duration);
+        target += units;
+        node.style[i] = target;
       }
-      node.style[i] = setvalue;
     }
   }
 
@@ -178,6 +246,7 @@
   }
 
   function expand(pros) {
+    //font,background直接翻译成backgroundColor 简写不支持
     var fixs = {
       'margin': function(val) {
         fixMP('margin', val, pros);
@@ -231,7 +300,7 @@
     }
     expand(this.queueItem.source);
     expand(this.queueItem.to);
-    setToStyle(this.queueItem.node, this.queueItem.source);
+    //setToStyle(this.queueItem.node, this.queueItem.source);
   }
 
   function now() {
@@ -252,7 +321,7 @@
     var t = this.endTime - now();
     if (this.currFrame === this.frames) {
       //有偏差->一次性设置成to得状态
-      setToStyle(item.node, item.to);
+      //setToStyle(item.node, item.to);
       if (item.cb) item.cb();
       setEnd.call(this);
       return;
@@ -325,14 +394,15 @@
         this.startTime = now();
         this.endTime = this.startTime + this.duration;
         for (var i in item.to) {
-         this.props[i] = item.to[i];
+          this.props[i] = item.to[i];
         }
         setVendorPreperty.call(this, 'transition-duration', this.duration);
         setVendorPreperty.call(this, 'transition-timing-function', item.ease);
         setCss3Style.call(this, item);
-        self.after = setTimeout(function(){
-            self.queueItem.cb && self.queueItem.cb(); 
-        },this.duration);
+        self.after = setTimeout(function() {
+          self.queueItem.cb && self.queueItem.cb();
+        },
+        this.duration);
       },
       restart: function() {
         setToStyle(this.queueItem.node, this.queueItem.source);
@@ -340,13 +410,15 @@
         this.start();
       },
       play: function() {
-        var item = this.queueItem,self = this;
+        var item = this.queueItem,
+        self = this;
         setVendorPreperty.call(this, 'transition-duration', this.endTime - this.stopTime);
         setVendorPreperty.call(this, 'transition-timing-function', item.ease);
         setCss3Style.call(this, item);
-        self.after = setTimeout(function(){
-            self.queueItem.cb && self.queueItem.cb(); 
-        },this.endTime - this.stopTime);
+        self.after = setTimeout(function() {
+          self.queueItem.cb && self.queueItem.cb();
+        },
+        this.endTime - this.stopTime);
         this.stopTime = 0;
       },
       stop: function() {
